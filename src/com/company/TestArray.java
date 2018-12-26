4e63911d05d810b5056a79a7ab9a80447eeff67f
toxober <toxober@gmail.com>
2018-12-26 12:27:39 +0200

    TestArray

--- src/com/company/TestArray.java
+++ src/com/company/TestArray.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 
 public class TestArray {
     public static void main(String args[]) throws IOException {
-        int guests[] = new int[10];
+        byte guests[] = new byte[10];
         Scanner diskScanner = new Scanner(new File("D:/GuestList.txt"));
 
-        for(int roomNum = 0; roomNum < 10; roomNum++) {
-            guests[roomNum] = diskScanner.nextInt();
+        for(byte roomNum = 0; roomNum < 10; roomNum++) {
+            guests[roomNum] = diskScanner.nextByte();
         }
         out.println("room\tnumbers");
-        for(int numGuest : guests){ //Альтернативный способ записи цикла: Тип_переменной Имя_переменной : Диапазон_значений (указал что roomNum = 0 и при выводе добавить ++)
+        for(int numGuest : guests){ //Альтернативный способ записи цикла: Тип_переменной Имя_переменной : Диапазон_значений (указал что roomNum = 0 и при выводе добавить ++) И ещё, при использовании "расширенного" цикла for в каждой итерации в переменной цикла numGuest сохраняется копия одного из значений диапазона guests и переменная numGuest не указывает ни на диапазон, ни на его текущее значение.
             int roomNum= 0;
             out.print(roomNum++);
             out.print("\t");
