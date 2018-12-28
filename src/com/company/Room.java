4d695e3fb8191d969423457c32a2d01efae916d4
anton zabolotny <toxober@gmail.com>
2018-12-28 09:18:10 +0200

    Del77

---
+++ src/com/company/Room.java
@@ -0,0 +1,24 @@
+package com.company;
+import static java.lang.System.out;
+import java.util.Locale;
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class Room {
+    private int guests;
+    private double rate;
+    private boolean smoking;
+    private static NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.US);
+public void readRoom(Scanner diskScanner) {
+    guests = diskScanner.nextInt();
+    rate = diskScanner.nextDouble();
+    smoking = diskScanner.nextBoolean();
+    }
+    public void writeRoom(){
+    out.print(guests);
+    out.print("/t");
+    out.print(currency.format(rate));
+    out.print("/t/t");
+    out.print(smoking ? "yes" : "no");
+    }
+}
